cf7dd3c730caf0ddf973be39b2426a22fc4657aa
Nguyen Hieu <nguuyenminhhieu28801@gmail.com>
2023-02-13 16:25:07 +0700

    Enterpise

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import ServiceEngineering from "./Page/Service-Engineering/ServiceEngineering";
 import Integration from "./Page/Service-Integration/Integration";
 import Blockchain from "./Page/Blockchain/Blockchain";
+import Enterprise from "./Page/Enterprise/Enterprise";
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
         <Route path="/engineering" element={<ServiceEngineering />} />
         <Route path="/integration" element={<Integration />} />
         <Route path="/blockchain" element={<Blockchain />} />
+        <Route path="/enterprise" element={<Enterprise />} />
       </Routes>
     </div>
   );

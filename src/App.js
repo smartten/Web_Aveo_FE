f50a9cefbf705adbacb78e805d5e4cd757ae7ace
luong10 <buithiluong.10@gmail.com>
2023-02-14 22:55:03 +0700

    reponsive

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import Headerr from "./components/Header/Headerr.js";
 import Footer from "./components/Footer/Footer.js";
 // import CmcService from "./components/CMC_Service/CmcService.js";
 // import Testing from "./components/Testing/Testing.js";
-// import Ecommerce from "./components/Ecommerce/Ecommerce.js";
+import Ecommerce from "./components/Ecommerce/Ecommerce.js";
 
 import React, { useEffect } from "react";
 
 function App() {
   return (
     <>
-      {/* <Headerr /> */}
+      <Ecommerce />
       <Footer />
     </>
   );

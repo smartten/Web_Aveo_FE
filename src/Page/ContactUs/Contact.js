56e57215e792d7c9f7e761a8fd8bcaa154494d3a
Nguyen Hieu <nguuyenminhhieu28801@gmail.com>
2023-02-17 10:41:53 +0700

    fix contact

--- src/Page/ContactUs/Contact.js
+++ src/Page/ContactUs/Contact.js
@@ -102,7 +102,7 @@ function Contact() {
                 >
                   <Form.Item
                     label="This question is about:"
-                    name="name"
+                    name="question"
                     rules={[
                       {
                         required: true,
@@ -153,7 +153,7 @@ function Contact() {
                   </Form.Item>
                   <Form.Item
                     label="Your Company Name"
-                    name="name"
+                    name="Company"
                     rules={[
                       {
                         required: true,
